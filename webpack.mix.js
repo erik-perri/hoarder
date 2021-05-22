8ac471ee66cd23bba85c705aff3fde7f47681b0a
Erik Perri <erik.perri@gmail.com>
2021-05-22 17:55:02 -0400

    build: Made dev server host and port configurable and changed default port to be less common

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,11 @@
 const mix = require('laravel-mix');
 const webpack = require('webpack');
 
+const {
+  WEBPACK_DEV_SERVER_HOST = 'localhost',
+  WEBPACK_DEV_SERVER_PORT = 18081,
+} = process.env;
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,14 +17,25 @@ const webpack = require('webpack');
  |
  */
 
-mix.ts('resources/js/app.ts', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
-    .vue()
-    .webpackConfig({
-        plugins: [
-            new webpack.DefinePlugin({
-                __VUE_OPTIONS_API__: true,
-                __VUE_PROD_DEVTOOLS__: false,
-            }),
-        ],
-    });
+mix
+  .ts('resources/js/app.ts', 'public/js')
+  .sass('resources/scss/app.scss', 'public/css')
+  .vue()
+  .options({
+    hmrOptions: {
+      host: WEBPACK_DEV_SERVER_HOST,
+      port: WEBPACK_DEV_SERVER_PORT,
+    },
+  })
+  .webpackConfig({
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+    ],
+    devServer: {
+      host: WEBPACK_DEV_SERVER_HOST,
+      port: WEBPACK_DEV_SERVER_PORT,
+    },
+  });
