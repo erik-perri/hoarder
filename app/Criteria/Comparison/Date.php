a9ced8bceff48972cddf0127bdbbec9fb9b6bcff
Erik Perri <erik.perri@gmail.com>
2021-05-31 21:00:47 -0400

    fix: Added missing date comparison types

--- app/Criteria/Comparison/Date.php
+++ app/Criteria/Comparison/Date.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Query\Builder;
 class Date implements ComparisonInterface
 {
     public const COMPARISON_ON = 'date_on';
+    public const COMPARISON_ON_OR_AFTER = 'date_on_or_after';
     public const COMPARISON_AFTER = 'date_after';
+    public const COMPARISON_ON_OR_BEFORE = 'date_on_or_before';
     public const COMPARISON_BEFORE = 'date_before';
 
     public function apply(Builder $builder, bool $or, string $column, string $comparison, string $value): void
@@ -16,8 +18,14 @@ class Date implements ComparisonInterface
             case static::COMPARISON_ON:
                 $operator = '=';
                 break;
+            case static::COMPARISON_ON_OR_AFTER:
+                $operator = '>=';
+                break;
             case static::COMPARISON_AFTER:
                 $operator = '>';
+                break;
+            case static::COMPARISON_ON_OR_BEFORE:
+                $operator = '<=';
                 break;
             case static::COMPARISON_BEFORE:
                 $operator = '<';
