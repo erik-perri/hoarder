27dd91e71208543243535cd35e843231a5c13e32
Erik Perri <erik.perri@gmail.com>
2021-06-05 12:00:11 -0400

    refactor: Made response source more clear

--- resources/js/api/types.ts
+++ resources/js/api/types.ts
@@ -1,13 +1,13 @@
-// TODO Build a class on the server side to return a consistent API response
-
-export interface ApiResponse<DataType = never> {
-  status: 'success' | 'fail' | 'error';
-  message?: string;
-  errors?: Record<string, string[]>;
-  data?: DataType;
-}
-
+// This is what Laravel returns when a Request validation fails
 export interface FormErrors {
   message: string;
   errors: Record<string, string[]>;
+}
+
+// This is what our API requests should return
+// TODO Build a class on the server side to return a consistent API response and update this to
+//      match.
+export interface ApiResponse<DataType = never> extends Partial<FormErrors> {
+  status: 'success' | 'fail' | 'error';
+  data?: DataType;
 }
